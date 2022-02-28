52a9f4bbed080b72a17121230d545ab549954b23
CassioCarvalhoSilva <cassiocarvalhosilva7@gmail.com>
2022-02-28 12:44:18 -0300

    Acrescentada a mensagem desejando um bom dia

--- Projeto teste/Program.cs
+++ Projeto teste/Program.cs
@@ -7,8 +7,8 @@ namespace Course
         static void Main(string[] args)
         {
             Console.WriteLine("Ola mundo!");
-            Console.WriteLine("Bom dia!");
             Console.WriteLine("Como voce está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
         }
